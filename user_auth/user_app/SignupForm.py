d2bd21e2b7a391f1f3921959f1cc137743525fa9
hareshwar <hareshawrmali20@gmail.com>
2025-01-29 10:49:54 +0530

    create form for signup

---
+++ user_auth/user_app/SignupForm.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User
+
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'mobile', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+
+    def clean_mobile(self):
+        mobile = self.cleaned_data.get('mobile')
+        if len(mobile) != 10:  # Simple validation for mobile number length
+            raise forms.ValidationError('Mobile number must be 10 digits.')
+        return mobile
