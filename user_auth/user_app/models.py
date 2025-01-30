dac1fe04bf4dc1ceaab0f71908413d336dff9054
Hareshwar <hareshwarmali20@gmail.com>
2025-01-30 06:59:28 +0530

    update model fields

--- user_auth/user_app/models.py
+++ user_auth/user_app/models.py
@@ -2,21 +2,11 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # Adding mobile and verified fields
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     mobile = models.CharField(max_length=16, unique=True, null=True, blank=True)
-    is_verified = models.BooleanField(default=False)
-
-    # Fixing clash with reverse accessors
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='user_app_users',  # Add a custom related_name
-        blank=True
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='user_app_users',  # Add a custom related_name
-        blank=True
-    )
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)  # To track email verification
 
     def __str__(self):
         return self.username
